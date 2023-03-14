6304ccd7b2e3f722b10207556f96c8b00680d271
Joe Kunk <jkunk@dewpoint.com>
2023-03-13 21:54:05 -0400

    Clean up Adventureworks2012 for demos

--- 2023-03-Orlando/Clean AdventureWorks2012 for demos.sql
+++ 2023-03-Orlando/Clean AdventureWorks2012 for demos.sql
@@ -1,30 +1,44 @@
 USE AdventureWorks2012;
 GO
 
+-- Clean up the database from DB Role (HRPerson) demo
+IF EXiSTS( SELECT 1 FROM sys.database_principals WHERE name = 'xrHR_Address' AND type = 'R') BEGIN
+    ALTER ROLE xrHR_Addresss DROP MEMBER HRPerson
+END
+GO
+
 -- Clean up the database from prior demo runs
 IF EXiSTS( SELECT 1 FROM sys.database_principals WHERE name = 'xrAuditors' AND type = 'R') BEGIN
     ALTER ROLE xrAuditors DROP MEMBER AuditorPerson
     ALTER ROLE xrAuditors DROP MEMBER AuditManager
 END
 GO
+
 IF EXiSTS( SELECT 1 FROM sys.database_principals WHERE name = 'xrAuditors_Admin' AND type = 'R') BEGIN
     ALTER ROLE xrAuditors_Admin DROP MEMBER AuditManager
 END
+GO
 
 DROP USER IF EXISTS AuditPerson
 DROP USER IF EXISTS AuditManager
+GO
 
 IF EXISTS (SELECT 1 FROM master.sys.server_principals WHERE name = 'AuditorPerson') BEGIN
     DROP LOGIN AuditorPerson -- drop from server
 END
 GO
+
 IF EXISTS (SELECT 1 FROM master.sys.server_principals WHERE name = 'AuditManager') BEGIN
     DROP LOGIN AuditManager -- drop from server
 END
+GO
 
 DROP ROLE IF EXiSTS xrAuditors
 DROP ROLE IF EXiSTS xrAuditors_Admin
+GO
 
 DROP TABLE IF EXISTS Auditors.HelloAuditorsSchema
+GO
 
 DROP SCHEMA IF EXISTS Auditors
+GO
