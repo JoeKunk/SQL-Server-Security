ab4dd68e35cb81f3e153833e84179d3c0a1f0107
Joe Kunk <jkunk@dewpoint.com>
2023-03-13 21:54:37 -0400

    Set up Bikestores databases for Demo

---
+++ 2023-03-Orlando/Create View BikeStoresUSA.sales.vCustomerCountByCity.sql
@@ -0,0 +1,18 @@
+USE BikeStoresUSA
+GO
+
+IF object_id('sales.vCustomerCountByCity', 'V') IS NOT NULL
+	DROP VIEW sales.vCustomerCountByCity
+GO
+
+CREATE VIEW sales.vCustomerCountByCity AS
+	Select 'USA' as Country, City, count(customer_id) as [Customer Count] 
+	FROM BikeStoresUSA.Sales.Customers 
+	GROUP BY City
+	
+	UNION
+	
+	Select 'CAN' as Country, City, count(customer_id) as [Customer Count] 
+	FROM BikeStoresUSA.Sales.Customers 
+	GROUP BY City
+	
